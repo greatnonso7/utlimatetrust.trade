cd4f6f67a49f884473a672cf0d4bde05803dae4d
Ichoku Chinonso <greatchinonso7@gmail.com>
2021-06-14 22:28:51 +0100

    WIP finishing up frontend

---
+++ done.php
@@ -0,0 +1,37 @@
+<?php include('includes/head.php'); ?>
+
+<body>
+    <?php include('includes/header.php'); ?>
+    <?php include('includes/ticker.php'); ?>
+
+    <div id="main-other">
+        <div id="sub-other">
+            <div class="other-head">
+                <h1>Fidelity Trade Ltd</h1>
+            </div>
+        </div>
+    </div>
+    <div id="main-reg">
+        <div id="sub-contact">
+            <!--id change-->
+
+
+            <h3>Registration completed:</h3><br>
+
+            Thank you for joining our program.<br>
+            You are now an official member of this program. You can login to your account to start investing with us and use all the services that are available for our members.
+            <br>
+            <br>
+
+            <b>Important:</b> Do not provide your login and password to anyone!
+
+        </div>
+    </div>
+    </div>
+
+
+
+
+
+
+    <?php include('includes/footer.php'); ?>
