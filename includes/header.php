f081e78f41b21095feba942f5bdcc42ac5753028
Ichoku Chinonso <greatchinonso7@gmail.com>
2021-06-20 20:33:47 +0100

    WIP making investment

--- includes/header.php
+++ includes/header.php
@@ -86,3 +86,59 @@
     </nav>
   </div>
 </div>
+
+<style>
+  .widget_horizontal_container {
+    width: 100%;
+    height: 40px;
+    background-color: #FFFFFF;
+    overflow: hidden;
+    box-sizing: border-box;
+    border: 1px solid #56667F;
+    border-radius: 4px;
+    text-align: right;
+    line-height: 14px;
+    block-size: 40px;
+    font-size: 12px;
+    box-sizing: content-box;
+    font-feature-settings: normal;
+    text-size-adjust: 100%;
+    box-shadow: inset 0 -20px 0 0 #56667F;
+    padding: 1px;
+    padding: 0px;
+    margin: 0px;
+  }
+
+  .widget_horizontal_iframe_container {
+    height: 42px;
+    width: calc(100% - 1px);
+  }
+
+  .widget_horizontal_iframe {
+    border: 0;
+    margin: 0;
+    padding: 0;
+  }
+
+  .widget_horizontal_powered {
+    color: #FFFFFF;
+    font-weight: 400;
+    font-size: 11px;
+    box-sizing: content-box;
+    margin: 3px 6px 3px 0px;
+    font-family: Verdana, Tahoma, Arial, sans-serif;
+    float: right !important;
+  }
+
+  .widget_horizontal_powered_link {
+    font-weight: 500;
+    color: #FFFFFF !important;
+    text-decoration: none;
+    font-size: 11px;
+    display: inline-block;
+    margin-right: 5px;
+  }
+</style>
+<div class="widget_horizontal_container">
+  <div class="widget_horizontal_iframe_container"><iframe src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=light&pref_coin_id=1505&invert_hover=no" width="100%" height="100%" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" class="widget_horizontal_iframe"></iframe></div>
+</div>
